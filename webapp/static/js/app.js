666d06491a012aa0c2bd080d26fb6e7fa01a0335
Hugues Demers <hdemers@gmail.com>
2012-05-05 19:16:13 -0400

    Add a demo bar chart using D3.js

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -8,14 +8,16 @@ define([
   "knockout",
   "viewmodel",
   "graph",
+  "viz",
   "bootstrap"
 ],
-function ($, _, ko, viewmodel, graph) {
+function ($, _, ko, viewmodel, graph, viz) {
   var exports = {};
 
   exports.initialize = function () {
     console.log("Initializing app.");
     graph.initialize("graphContainer1");
+    viz.initialize();
     ko.applyBindings(viewmodel);
   };
   return exports;
