a84a802faecaee66495ae39c38eb708d4149a6f7
Hugues Demers <hdemers@gmail.com>
2012-06-14 09:14:40 -0400

    Deploy to Heroku.

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
+import os
+
 from webapp import app
 
-
 #os.environ["FLASK_DEV_SERVER"] = "true"
-app.run(debug=True, host='0.0.0.0')
+
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
